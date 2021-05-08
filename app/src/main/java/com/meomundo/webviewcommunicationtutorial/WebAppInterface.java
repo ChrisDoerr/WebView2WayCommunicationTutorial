6cdda204596d17b911fd61ce3830a3bab1b357cc
Chris Doerr <doerr@meomundo.com>
2021-05-08 10:57:08 +0200

    WebAppInterface class to handle incoming requests.

---
+++ app/src/main/java/com/meomundo/webviewcommunicationtutorial/WebAppInterface.java
@@ -0,0 +1,18 @@
+package com.meomundo.webviewcommunicationtutorial;
+
+import android.content.Context;
+
+public class WebAppInterface {
+
+    private Context context;
+
+    private MainActivity mainActivity;
+
+    public WebAppInterface( Context context, MainActivity mainActivity ) {
+
+        this.context        = context;
+        this.mainActivity   = mainActivity;
+
+    }
+
+}
