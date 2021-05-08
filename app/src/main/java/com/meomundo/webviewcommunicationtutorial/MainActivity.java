413802c8072fe846371b70d608655ed9a0407988
Chris Doerr <doerr@meomundo.com>
2021-05-08 10:50:34 +0200

    Add WebView in the layout + set it up in the code

--- app/src/main/java/com/meomundo/webviewcommunicationtutorial/MainActivity.java
+++ app/src/main/java/com/meomundo/webviewcommunicationtutorial/MainActivity.java
@@ -2,13 +2,26 @@ package com.meomundo.webviewcommunicationtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate( Bundle savedInstanceState ) {
+
+        super.onCreate( savedInstanceState );
+
+        setContentView( R.layout.activity_main );
+
+        webView = findViewById( R.id.mainWebView );
+
+        webView.setWebViewClient( new WebViewClient() );
+
     }
+
 }
